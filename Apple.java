464692461e5187648f0ea0ba9850256002b83b90
TuRtLeMaStEr <335672952@gapps.yrdsb.ca>
2022-12-01 12:48:41 -0500

    dec1

--- Apple.java
+++ Apple.java
@@ -1,20 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Elphant the Hero
+ * Food for elephant
  * 
  * @author Kavin 
  * @version November 2022
  */
-public class Elephant extends Actor
+public class Apple extends Actor
 {
     /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         // Add your action code here.
-        move(1);
+        setLocation(getX(), getY() + 1);
     }
 }
